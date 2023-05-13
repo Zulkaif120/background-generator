420e1eec0b4df6ede6679c1fe4139614df74bbe2
Ahmad <zulkaifa@amazon.com>
2023-05-13 08:45:19 +0530

    initial commit

---
+++ component/card.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "./card.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import cls from "classnames";
+function Card(props) {
+  return (
+    <Link href={props.href} className={styles.cardLink}>
+      <div className={cls("glass", styles.container)}>
+        <div className={styles.cardHeaderWrapper}>
+          <h2 className={styles.cardHeader}>{props.name}</h2>
+        </div>
+        <div className={styles.cardImageWrapper}>
+          <Image
+            className={styles.cardImage}
+            src={props.imgUrl}
+            width={260}
+            height={160}
+          ></Image>
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+export default Card;
